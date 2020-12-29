fd8b1682ecbb850e2bc4e11f1b1bbf12203a9d35
Thinh Nguyen <thinhnguyen18082401@gmail.com>
2020-12-28 20:15:43 -0800

    - Allow the app to have the ability to eslize many folders at a time

--- gui.py
+++ gui.py
@@ -3,16 +3,44 @@ from convertESL import convert_esl
 root = Tk()
 root.title("ESLIZE YOUR FOLLOWERS")
 
+form = []
+i = 1
+
+# Define class entry box
+class EntryBox:
+    def __init__(self):
+        self.entry_formID = Entry(root, width=50, borderwidth=5)
+        self.entry_path = Entry(root, width=50, borderwidth=5)
+    def put_on_screen(self, index):
+        self.entry_formID.grid(row=index, column=0)
+        self.entry_formID.insert(0, "Enter your new form-ID")
+        self.entry_path.grid(row=index, column=1)
+        self.entry_path.insert(0, "Enter your follower's folder path")
+
+
+
 # Entry boxes
-entry_new_form_ID = Entry(root, width=50, borderwidth=5)
-entry_new_form_ID.grid(row=0, column=0)
-entry_new_form_ID.insert(0, "Enter your new form-ID")
-entry_follower_folder_path = Entry(root, width=50, borderwidth=5)
-entry_follower_folder_path.grid(row=0, column=1)
-entry_follower_folder_path.insert(0, "Enter your follower's folder path")
+box1 = EntryBox()
+box1.put_on_screen(0)
+form.append(box1)
+
+# add entry boxes functions
+def add_box():
+    global i
+    newBox = EntryBox()
+    newBox.put_on_screen(i)
+    i += 1
+    form.append(newBox)
+
+def execute_program():
+    for items in form:
+        convert_esl(items.entry_formID.get(), items.entry_path.get())
 
 # Button to execute the programm
-button_execute_eslize = Button(root, text="Eslize the follower", padx=20, pady=20, command=lambda: convert_esl(entry_new_form_ID.get(), entry_follower_folder_path.get()))
-button_execute_eslize.grid(row=1, column=0)
+button_execute_eslize = Button(root, text="Eslize the follower", padx=20, pady=20, command=execute_program)
+button_execute_eslize.grid(row=0, column=2)
+
+button_new = Button(root, text="Create new box", padx=20, pady=20, command=add_box)
+button_new.grid(row=1, column=2)
 
 root.mainloop()
